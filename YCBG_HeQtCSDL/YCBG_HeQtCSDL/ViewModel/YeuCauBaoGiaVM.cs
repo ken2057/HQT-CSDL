19e4c82b0b5dd3b20dc1a60497d9c18a725adf52
NP712N <npn19998@gmail.com>
2019-11-13 11:31:11 +0700

    Init exec Stored Procedure from SQL

---
+++ YCBG_HeQtCSDL/YCBG_HeQtCSDL/ViewModel/YeuCauBaoGiaVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YCBG_HeQtCSDL.ViewModel
+{
+    public class YeuCauBaoGiaVM
+    {
+        [DisplayName("Mã yêu cầu báo giá")]
+        public string MaYCBG { get; set; }
+        [DisplayName("Ngày")]
+        public string NgayYCBG { get; set; }
+        [DisplayName("Tình trạng")]
+        public string TinhTrang { get; set; }
+        [DisplayName("Nhân viên")]
+        public string MaNV { get; set; }
+    }
+}
---
+++ YCBG_HeQtCSDL/YCBG_HeQtCSDL/main_YCBG.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using YCBG_HeQtCSDL.ViewModel;
+
+namespace YCBG_HeQtCSDL
+{
+    /// <summary>
+    /// Interaction logic for main_YCBG.xaml
+    /// </summary>
+    public partial class main_YCBG : Window
+    {
+        public main_YCBG()
+        {
+            InitializeComponent();
+
+            YeuCauBaoGiaVM yeuCauBaoGiaVM = new YeuCauBaoGiaVM();
+            List<YeuCauBaoGiaVM> yeuCauBaoGiaVMs = new List<YeuCauBaoGiaVM>();
+            SqlDataReader rdr = null;
+
+            using (var conn = new SqlConnection("data source=.;initial catalog=qlmuahang;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework"))
+            using (var command = new SqlCommand("sp_test", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            })
+            {
+                conn.Open();
+                try
+                {
+                    command.Parameters.Add(new SqlParameter("@flag", "1"));
+                    rdr = command.ExecuteReader();
+                    
+                    while (rdr.Read())
+                    {
+                        yeuCauBaoGiaVM = new YeuCauBaoGiaVM();
+                        yeuCauBaoGiaVM.MaYCBG = rdr["MaYCBaoGia"].ToString();
+                        yeuCauBaoGiaVM.NgayYCBG = rdr["NgayYCBaoGia"].ToString();
+                        yeuCauBaoGiaVM.TinhTrang = rdr["TinhTrang"].ToString();
+                        yeuCauBaoGiaVM.MaNV = rdr["MaNV"].ToString();
+                        yeuCauBaoGiaVMs.Add(yeuCauBaoGiaVM);
+                    }
+                    dtgYCBG.ItemsSource = yeuCauBaoGiaVMs;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                    throw;
+                }
+                
+                conn.Close();
+            }
+        }
+    }
+}
